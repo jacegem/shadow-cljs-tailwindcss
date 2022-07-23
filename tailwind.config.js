fccc6b84092dee0c1d28f73ba973caf5f38b9a05
Jiyong Kwon <jiyong.kwon@greenlabs.co.kr>
2022-07-23 17:02:03 +0900

    Change to docs folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
-  content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./public/js/cljs-runtime/*.js"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
+    // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
+    // content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./public/js/cljs-runtime/*.js"],
+    content: process.env.NODE_ENV == 'production' ? ["./docs/js/main.js"] : ["./docs/js/cljs-runtime/*.js"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
